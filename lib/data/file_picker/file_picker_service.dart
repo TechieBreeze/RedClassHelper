66808cc66f3881f08b883494d35594242532016f
红石 业余 <1336873574@qq.com>
2026-06-25 21:01:20 +0800

    feat(file_picker): add FilePickerService interface and Fake

---
+++ lib/data/file_picker/file_picker_service.dart
@@ -0,0 +1,7 @@
+import 'file_picker_models.dart';
+
+abstract interface class FilePickerService {
+  Future<PickedFile?> pickFile({required Set<String> allowedExtensions, String? dialogTitle});
+  Future<PickedFile?> pickFromDroppedPath(String path);
+  Future<void> dispose();
+}
---
+++ test/unit/data/file_picker/fake_file_picker_service.dart
@@ -0,0 +1,23 @@
+import 'dart:typed_data';
+import 'package:redclass/data/file_picker/file_picker_service.dart';
+import 'package:redclass/data/file_picker/file_picker_models.dart';
+
+class FakeFilePickerService implements FilePickerService {
+  PickedFile? nextResult;
+  Object? nextError;
+  final List<({Set<String> extensions, String? title})> calls = [];
+
+  @override
+  Future<PickedFile?> pickFile({required Set<String> allowedExtensions, String? dialogTitle}) async {
+    calls.add((extensions: allowedExtensions, title: dialogTitle));
+    if (nextError != null) throw nextError!;
+    return nextResult;
+  }
+  @override
+  Future<PickedFile?> pickFromDroppedPath(String path) async => null;
+  @override
+  Future<void> dispose() async {}
+}
+
+PickedFile fakePdfFile({String name = 'test.pdf'}) =>
+    PickedBytesFile(name: name, bytes: Uint8List.fromList([1, 2, 3]));
